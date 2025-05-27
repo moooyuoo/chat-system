108cb96c282cac5e55fbee326cda680fa4d1dc57
starlight-y <3428316227@qq.com>
2025-05-27 22:33:48 +0800

    Initial commit

---
+++ src/test/java/org/example/chatsystem/ChatSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chatsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
